45285183bc7ef3eb1080a89ea0d7b0b07adbcf0d
Zahid Hossain <60911932+zhossain-info@users.noreply.github.com>
2023-09-23 20:44:56 +0600

    Add test cases for the integer data type

---
+++ src/test/java/com/relogiclabs/json/schema/negative/IntegerTests.java
@@ -0,0 +1,233 @@
+package com.relogiclabs.json.schema.negative;
+
+import com.relogiclabs.json.schema.JsonAssert;
+import com.relogiclabs.json.schema.JsonSchema;
+import com.relogiclabs.json.schema.exception.JsonSchemaException;
+import org.junit.jupiter.api.Test;
+
+import static com.relogiclabs.json.schema.message.ErrorCode.DTYP04;
+import static com.relogiclabs.json.schema.message.ErrorCode.DTYP06;
+import static com.relogiclabs.json.schema.message.ErrorCode.FUNC06;
+import static com.relogiclabs.json.schema.message.ErrorCode.INTE01;
+import static com.relogiclabs.json.schema.message.ErrorCode.NEGI01;
+import static com.relogiclabs.json.schema.message.ErrorCode.POSI01;
+import static com.relogiclabs.json.schema.message.ErrorCode.RANG01;
+import static com.relogiclabs.json.schema.message.ErrorCode.RANG04;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntegerTests {
+    @Test
+    public void When_JsonNotInteger_ExceptionThrown() {
+        var schema = "#integer";
+        var json = "10.5";
+
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(DTYP04, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_JsonValueNotEqualForInteger_ExceptionThrown() {
+        var schema = "10 #integer";
+        var json = "9";
+
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(INTE01, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_JsonNotIntegerInObject_ExceptionThrown() {
+        var schema =
+            """
+            {
+                "key1": #integer,
+                "key2": #integer,
+                "key3": #integer
+            }
+            """;
+        var json =
+            """
+            {
+                "key1": null,
+                "key2": "value1",
+                "key3": 4000.45
+            }
+            """;
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(DTYP04, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_JsonNotIntegerInArray_ExceptionThrown() {
+        var schema =
+            """
+            [#integer, #integer, #integer]
+            """;
+        var json =
+            """
+            [true, -4568.57, 100]
+            """;
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(DTYP04, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_NestedJsonNotIntegerInArray_ExceptionThrown() {
+        var schema =
+            """
+            #integer*
+            """;
+        var json =
+            """
+            [null, 2.2, "40000000"]
+            """;
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(DTYP06, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_NestedJsonNotIntegerInObject_ExceptionThrown() {
+        var schema =
+            """
+            #integer*
+            """;
+        var json =
+            """
+            {
+                "key1": "value1",
+                "key2": false,
+                "key3": "-50000"
+            }
+            """;
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(DTYP06, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_NestedRangeWithJsonNotIntegerInObject_ExceptionThrown() {
+        var schema =
+            """
+            @range*(-100, 200) #integer*
+            """;
+        var json =
+            """
+            {
+                "key1": "value1",
+                "key2": false,
+                "key3": "-50000"
+            }
+            """;
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(DTYP06, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_NestedRangeWithNonCompositeJsonInObject_ExceptionThrown() {
+        var schema =
+            """
+            @range*(100, !)
+            """;
+        var json =
+            """
+            "value1"
+            """;
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(FUNC06, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_NestedRangeWithJsonWrongIntegerInObject_ExceptionThrown() {
+        var schema =
+            """
+            @range*(-100, 100) #integer*
+            """;
+        var json =
+            """
+            {
+                "key1": -100,
+                "key2": 100,
+                "key3": -500
+            }
+            """;
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(RANG01, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_NestedRangeWithUndefinedAndWrongIntegerInArray_ExceptionThrown() {
+        var schema =
+            """
+            @range*(!, 400) #integer*
+            """;
+        var json =
+            """
+            [100, 500, 900]
+            """;
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(RANG04, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_NestedPositiveWithWrongIntegerInArray_ExceptionThrown() {
+        var schema =
+            """
+            @positive* #integer*
+            """;
+        var json =
+            """
+            [100, -500, 900]
+            """;
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(POSI01, exception.getCode());
+        exception.printStackTrace();
+    }
+
+    @Test
+    public void When_NestedNegativeWithWrongIntegerInArray_ExceptionThrown() {
+        var schema =
+            """
+            @negative* #integer*
+            """;
+        var json =
+            """
+            [-100, -500, 900]
+            """;
+        JsonSchema.isValid(schema, json);
+        var exception = assertThrows(JsonSchemaException.class,
+                () -> JsonAssert.isValid(schema, json));
+        assertEquals(NEGI01, exception.getCode());
+        exception.printStackTrace();
+    }
+}
---
+++ src/test/java/com/relogiclabs/json/schema/positive/IntegerTests.java
@@ -0,0 +1,150 @@
+package com.relogiclabs.json.schema.positive;
+
+import com.relogiclabs.json.schema.JsonAssert;
+import org.junit.jupiter.api.Test;
+
+public class IntegerTests {
+    @Test
+    public void When_DataTypeInteger_ValidTrue() {
+        var schema = "#integer";
+        var json = "10";
+        JsonAssert.isValid(schema, json);
+    }
+
+    @Test
+    public void When_DataTypeIntegerInObject_ValidTrue() {
+        var schema =
+            """
+            {
+                "key1": #integer,
+                "key2": #integer,
+                "key3": #integer
+            }
+            """;
+        var json =
+            """
+            {
+                "key1": 0,
+                "key2": 5000,
+                "key3": 500000
+            }
+            """;
+        JsonAssert.isValid(schema, json);
+    }
+
+    @Test
+    public void When_DataTypeIntegerInArray_ValidTrue() {
+        var schema =
+            """
+            [#integer, #integer, #integer]
+            """;
+        var json =
+            """
+            [10, -20000, 40000000]
+            """;
+        JsonAssert.isValid(schema, json);
+    }
+
+    @Test
+    public void When_NestedDataTypeIntegerInArray_ValidTrue() {
+        var schema =
+            """
+            #integer*
+            """;
+        var json =
+            """
+            [0, -20000, 40000000]
+            """;
+        JsonAssert.isValid(schema, json);
+    }
+
+    @Test
+    public void When_NestedDataTypeIntegerInObject_ValidTrue() {
+        var schema =
+            """
+            @range*(-50000, 15000) #integer*
+            """;
+        var json =
+            """
+            {
+                "key1": -500,
+                "key2": 15000,
+                "key3": -50000
+            }
+            """;
+        JsonAssert.isValid(schema, json);
+    }
+
+    @Test
+    public void When_NestedRangeIntegerInObject_ValidTrue() {
+        var schema =
+            """
+            @range*(100, 200) #integer*
+            """;
+        var json =
+            """
+            {
+                "key1": 100,
+                "key2": 150,
+                "key3": 200
+            }
+            """;
+        JsonAssert.isValid(schema, json);
+    }
+
+    @Test
+    public void When_NestedRangeWithUndefinedIntegerInObject_ValidTrue() {
+        var schema =
+            """
+            @range*(100, !) #integer*
+            """;
+        var json =
+            """
+            {
+                "key1": 100,
+                "key2": 1500,
+                "key3": 200000
+            }
+            """;
+        JsonAssert.isValid(schema, json);
+    }
+
+    @Test
+    public void When_NestedRangeWithUndefinedIntegerInArray_ValidTrue() {
+        var schema =
+            """
+            @range*(!, 40000) #integer*
+            """;
+        var json =
+            """
+            [10, 2000, 40000]
+            """;
+        JsonAssert.isValid(schema, json);
+    }
+
+    @Test
+    public void When_NestedPositiveWithIntegerInArray_ValidTrue() {
+        var schema =
+            """
+            @positive* #integer*
+            """;
+        var json =
+            """
+            [100, 500, 900]
+            """;
+        JsonAssert.isValid(schema, json);
+    }
+
+    @Test
+    public void When_NestedNegativeWithIntegerInArray_ValidTrue() {
+        var schema =
+            """
+            @negative* #integer*
+            """;
+        var json =
+            """
+            [-100, -500, -900]
+            """;
+        JsonAssert.isValid(schema, json);
+    }
+}
