47b157eb6fbfa73a59ba8866bcbf9a4420686e60
Zahid Hossain <60911932+zhossain-info@users.noreply.github.com>
2023-09-23 19:25:00 +0600

    Add Json tree and tree visitor

---
+++ src/main/java/com/relogiclabs/json/schema/tree/JsonTree.java
@@ -0,0 +1,26 @@
+package com.relogiclabs.json.schema.tree;
+
+import com.relogiclabs.json.schema.internal.antlr.JsonLexer;
+import com.relogiclabs.json.schema.internal.antlr.JsonParser;
+import com.relogiclabs.json.schema.internal.tree.JsonTreeVisitor;
+import com.relogiclabs.json.schema.internal.util.LexerErrorListener;
+import com.relogiclabs.json.schema.internal.util.ParserErrorListener;
+import com.relogiclabs.json.schema.types.JRoot;
+import lombok.Getter;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+@Getter
+public class JsonTree {
+    private final JRoot root;
+
+    public JsonTree(RuntimeContext context, String input) {
+        var jsonLexer = new JsonLexer(CharStreams.fromString(input));
+        jsonLexer.removeErrorListeners();
+        jsonLexer.addErrorListener(LexerErrorListener.JSON);
+        var jsonParser = new JsonParser(new CommonTokenStream(jsonLexer));
+        jsonParser.removeErrorListeners();
+        jsonParser.addErrorListener(ParserErrorListener.JSON);
+        root = (JRoot) new JsonTreeVisitor(context).visit(jsonParser.json());
+    }
+}
