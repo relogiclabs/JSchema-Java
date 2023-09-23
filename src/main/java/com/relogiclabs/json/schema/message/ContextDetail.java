1ade09218586bc331acd995d39f061c73b31bf23
Zahid Hossain <60911932+zhossain-info@users.noreply.github.com>
2023-09-23 20:16:00 +0600

    Add error detail handlers for all exceptions

---
+++ src/main/java/com/relogiclabs/json/schema/message/ContextDetail.java
@@ -0,0 +1,23 @@
+package com.relogiclabs.json.schema.message;
+
+import com.relogiclabs.json.schema.tree.Context;
+import com.relogiclabs.json.schema.tree.Location;
+import com.relogiclabs.json.schema.types.JNode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ContextDetail {
+    private Context context;
+    private String message;
+
+    public ContextDetail(JNode node, String message) {
+        this.context = node.getContext();
+        this.message = message;
+    }
+
+    public Location getLocation() {
+        return context.getLocation();
+    }
+}
