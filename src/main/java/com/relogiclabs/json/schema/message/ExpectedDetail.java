b90b8bd041c090542d38318810e2ab73fa8ec685
Zahid Hossain <60911932+zhossain-info@users.noreply.github.com>
2023-09-23 20:08:57 +0600

    Add expected detail provider for assertion

---
+++ src/main/java/com/relogiclabs/json/schema/message/ExpectedDetail.java
@@ -0,0 +1,32 @@
+package com.relogiclabs.json.schema.message;
+
+import com.relogiclabs.json.schema.tree.Context;
+import com.relogiclabs.json.schema.types.JNode;
+
+import static com.relogiclabs.json.schema.internal.util.StringHelper.concat;
+
+public class ExpectedDetail extends ContextDetail {
+    public ExpectedDetail(Context context, String message) {
+        super(context, message);
+    }
+
+    public ExpectedDetail(JNode node, String message) {
+        super(node, message);
+    }
+
+    public ExpectedDetail(JNode node, Object m1, Object m2) {
+        this(node, concat(m1, m2));
+    }
+
+    public ExpectedDetail(JNode node, Object m1, Object m2, Object m3) {
+        this(node, concat(m1, m2, m3));
+    }
+
+    public ExpectedDetail(JNode node, Object m1, Object m2, Object m3, Object m4) {
+        this(node, concat(m1, m2, m3, m4));
+    }
+
+    public ExpectedDetail(JNode node, Object m1, Object m2, Object m3, Object m4, Object m5) {
+        this(node, concat(m1, m2, m3, m4, m5));
+    }
+}
