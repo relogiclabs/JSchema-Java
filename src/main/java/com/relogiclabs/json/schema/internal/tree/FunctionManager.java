6a889a939dd46a1cd87872d3469e9b3c4e3248a2
Zahid Hossain <60911932+zhossain-info@users.noreply.github.com>
2023-09-23 19:28:27 +0600

    Add class including handler

---
+++ src/main/java/com/relogiclabs/json/schema/internal/tree/FunctionManager.java
@@ -0,0 +1,207 @@
+package com.relogiclabs.json.schema.internal.tree;
+
+import com.relogiclabs.json.schema.exception.ClassInstantiationException;
+import com.relogiclabs.json.schema.exception.CommonException;
+import com.relogiclabs.json.schema.exception.DuplicateIncludeException;
+import com.relogiclabs.json.schema.exception.FunctionMismatchException;
+import com.relogiclabs.json.schema.exception.FunctionNotFoundException;
+import com.relogiclabs.json.schema.exception.InvalidFunctionException;
+import com.relogiclabs.json.schema.exception.InvalidIncludeException;
+import com.relogiclabs.json.schema.exception.NotFoundClassException;
+import com.relogiclabs.json.schema.function.FunctionBase;
+import com.relogiclabs.json.schema.message.MessageFormatter;
+import com.relogiclabs.json.schema.tree.Context;
+import com.relogiclabs.json.schema.tree.RuntimeContext;
+import com.relogiclabs.json.schema.types.JFunction;
+import com.relogiclabs.json.schema.types.JNode;
+import com.relogiclabs.json.schema.types.JsonType;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.relogiclabs.json.schema.internal.tree.MethodPointer.getSignature;
+import static com.relogiclabs.json.schema.internal.util.CollectionHelper.merge;
+import static com.relogiclabs.json.schema.internal.util.StringHelper.concat;
+import static com.relogiclabs.json.schema.message.ErrorCode.CLAS01;
+import static com.relogiclabs.json.schema.message.ErrorCode.CLAS02;
+import static com.relogiclabs.json.schema.message.ErrorCode.CLAS03;
+import static com.relogiclabs.json.schema.message.ErrorCode.CLAS04;
+import static com.relogiclabs.json.schema.message.ErrorCode.CLAS05;
+import static com.relogiclabs.json.schema.message.ErrorCode.CLAS06;
+import static com.relogiclabs.json.schema.message.ErrorCode.CLAS07;
+import static com.relogiclabs.json.schema.message.ErrorCode.FUNC01;
+import static com.relogiclabs.json.schema.message.ErrorCode.FUNC02;
+import static com.relogiclabs.json.schema.message.ErrorCode.FUNC03;
+import static com.relogiclabs.json.schema.message.ErrorCode.FUNC04;
+import static com.relogiclabs.json.schema.message.ErrorCode.FUNC05;
+
+public class FunctionManager {
+    private final Set<String> includes;
+    private final Map<FunctionKey, List<MethodPointer>> functions;
+    private final RuntimeContext runtime;
+
+    public FunctionManager(RuntimeContext runtime) {
+        this.runtime = runtime;
+        this.includes = new HashSet<>();
+        this.functions = new HashMap<>();
+    }
+
+    public void addClass(String className, Context context) {
+        if(!includes.contains(className)) includes.add(className);
+        else throw new DuplicateIncludeException(MessageFormatter.formatForSchema(
+                CLAS01, "Class already included " + className, context));
+        Class<?> subclass;
+        try {
+            subclass = Class.forName(className);
+        } catch(ClassNotFoundException ex) {
+            throw new NotFoundClassException(
+                    MessageFormatter.formatForSchema(CLAS02, "Not found " + className, context));
+        }
+        var baseclass = FunctionBase.class;
+        // if not FunctionBase's subclass
+        if(!baseclass.isAssignableFrom(subclass))
+            throw new InvalidIncludeException(MessageFormatter
+                    .formatForSchema(CLAS03, subclass.getName() + " needs to inherit "
+                            + baseclass.getName(), context));
+        try {
+            merge(functions, extractMethods(subclass, createInstance(subclass, context)));
+        } catch(InvalidFunctionException ex) {
+            throw new InvalidFunctionException(MessageFormatter
+                .formatForSchema(ex.getCode(), ex.getMessage(), context));
+        }
+    }
+
+    private Map<FunctionKey, List<MethodPointer>> extractMethods(Class<?> subclass, FunctionBase instance) {
+        var baseclass = FunctionBase.class;
+        Map<FunctionKey, List<MethodPointer>> functions = new HashMap<>();
+        for(var m : subclass.getMethods()) {
+            if(!baseclass.isAssignableFrom(m.getDeclaringClass())) continue;
+            if(baseclass == m.getDeclaringClass()) continue;
+            Parameter[] parameters = m.getParameters();
+            if(m.getReturnType() != boolean.class && m.getReturnType() != Boolean.class)
+                throw new InvalidFunctionException(FUNC01, concat("Function [", getSignature(m),
+                        "] requires return type boolean"));
+            if(parameters.length < 1) throw new InvalidFunctionException(FUNC02,
+                    concat("Function [", getSignature(m), "] requires minimum one parameter"));
+            var key = new FunctionKey(m, getParameterCount(parameters));
+            var value = new MethodPointer(instance, m, parameters);
+            var valueList = functions.get(key);
+            if(valueList == null) valueList = new ArrayList<>();
+            valueList.add(value);
+            functions.put(key, valueList);
+        }
+        return functions;
+    }
+
+    private int getParameterCount(Parameter[] parameters) {
+        for(var p : parameters) if(p.isVarArgs()) return -1;
+        return parameters.length;
+    }
+
+    private FunctionBase createInstance(Class<?> type, Context context) {
+        try {
+            var constructor = type.getDeclaredConstructor(RuntimeContext.class);
+            return (FunctionBase) constructor.newInstance(runtime);
+        } catch (NoSuchMethodException e) {
+            throw createException(CLAS04, e, type, context);
+        } catch (InstantiationException e) {
+            throw createException(CLAS05, e, type, context);
+        } catch (InvocationTargetException e) {
+            throw createException(CLAS06, e, type, context);
+        } catch (IllegalAccessException e) {
+            throw createException(CLAS07, e, type, context);
+        }
+    }
+
+    private static CommonException createException(String code, Exception ex, Class<?> type, Context context) {
+        return new ClassInstantiationException(MessageFormatter.formatForSchema(
+                code, "Fail to create instance of " + type.getName(), context), ex);
+    }
+
+    public boolean invokeFunction(JFunction function, JNode target) {
+        var methods = getMethods(function);
+        String mismatchMessage = null;
+        for(var method : methods) {
+            var parameters = method.getParameters();
+            var arguments = function.getArguments();
+            var schemaArgs = processArgs(parameters, arguments);
+            if(schemaArgs == null) continue;
+            if(!isMatch(parameters.get(0), target)) {
+                mismatchMessage = "Function %s is applicable on %s but applied on %s of %s"
+                        .formatted(function.getOutline(), getTypeName(parameters.get(0).getType()),
+                                getTypeName(target.getClass()), target);
+                continue;
+            }
+            return method.invoke(function, addTarget(schemaArgs, target));
+        }
+        if(mismatchMessage != null) return failWith(new FunctionMismatchException(
+            MessageFormatter.formatForSchema(FUNC03, mismatchMessage, function.getContext())));
+        return failWith(new FunctionNotFoundException(MessageFormatter
+            .formatForSchema(FUNC04, function.getOutline(), function.getContext())));
+    }
+
+    private static String getTypeName(Class<?> type) {
+        JsonType t = JsonType.from(type);
+        if(t != null) return t.getName();
+        else return type.getSimpleName();
+    }
+
+    private List<MethodPointer> getMethods(JFunction function) {
+        var methodPointers = functions.get(new FunctionKey(function));
+        if(methodPointers == null)
+            methodPointers = functions.get(new FunctionKey(function.getName(), -1));
+        if(methodPointers == null) throw new FunctionNotFoundException(MessageFormatter
+                .formatForSchema(FUNC05, "Not found " + function.getOutline(), function.getContext()));
+        return methodPointers;
+    }
+
+    private static List<Object> addTarget(List<Object> arguments, JNode target) {
+        var args = new ArrayList<>(1 + arguments.size());
+        args.add(target);
+        args.addAll(arguments);
+        return args;
+    }
+
+    private static List<Object> processArgs(List<Parameter> parameters, List<JNode> arguments) {
+        var result = new ArrayList<>();
+        for(int i = 1; i < parameters.size(); i++) {
+            if(parameters.get(i).isVarArgs()) {
+                List<JNode> rest = arguments.subList(i - 1, arguments.size() - i + 1);
+                var varArgs = processVarArgs(parameters.get(i), rest);
+                if(varArgs == null) return null;
+                result.add(varArgs);
+                break;
+            }
+            if(!isMatch(parameters.get(i), arguments.get(i - 1))) return null;
+            result.add(arguments.get(i - 1));
+        }
+        return result;
+    }
+
+    private static boolean isMatch(Parameter parameter, JNode argument) {
+        return parameter.getType().isInstance(argument);
+    }
+
+    private static Object processVarArgs(Parameter parameter, List<JNode> arguments) {
+        var componentType = parameter.getType().getComponentType();
+        if(componentType == null) throw new IllegalStateException();
+        Object result = Array.newInstance(componentType, arguments.size());
+        for(var i = 0; i < arguments.size(); i++) {
+            var arg = arguments.get(i);
+            if(!componentType.isInstance(arg)) return null;
+            Array.set(result, i, arg);
+        }
+        return result;
+    }
+
+    private boolean failWith(RuntimeException exception) {
+        return runtime.failWith(exception);
+    }
+}
---
+++ src/main/java/com/relogiclabs/json/schema/internal/tree/MethodPointer.java
@@ -0,0 +1,62 @@
+package com.relogiclabs.json.schema.internal.tree;
+
+import com.relogiclabs.json.schema.exception.TargetInvocationException;
+import com.relogiclabs.json.schema.function.FunctionBase;
+import com.relogiclabs.json.schema.types.JFunction;
+import lombok.Getter;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.relogiclabs.json.schema.internal.util.StringHelper.concat;
+import static com.relogiclabs.json.schema.message.ErrorCode.FUNC07;
+import static com.relogiclabs.json.schema.message.ErrorCode.FUNC08;
+import static java.util.stream.Collectors.joining;
+
+@Getter
+public class MethodPointer {
+
+    private final FunctionBase instance;
+    private final Method method;
+    public final List<Parameter> parameters;
+
+    public MethodPointer(FunctionBase instance, Method method, Parameter[] parameters) {
+        this.instance = instance;
+        this.method = method;
+        this.parameters = List.of(parameters);
+    }
+
+    public boolean invoke(JFunction function, List<Object> arguments) {
+        try {
+            instance.setFunction(function);
+            Object result = method.invoke(instance, arguments.toArray());
+            if(!(result instanceof Boolean boolResult)) throw new IllegalStateException();
+            return boolResult;
+        } catch (InvocationTargetException e) {
+            throw new TargetInvocationException(FUNC07,
+                    "Target invocation exception occurred", e.getCause());
+        } catch (IllegalAccessException e) {
+            throw new TargetInvocationException(FUNC08,
+                    "Illegal access exception occurred", e);
+        }
+    }
+
+    public static String getSignature(Method method) {
+        String typeName = method.getDeclaringClass().getName();
+        String methodName = method.getName();
+        String parameters = Arrays.stream(method.getParameters())
+                .map(MethodPointer::stringOf).collect(joining(", "));
+        String returnType = method.getReturnType().getName();
+        return concat(returnType, " ", typeName, ".", methodName, "(", parameters, ")");
+    }
+
+    private static String stringOf(Parameter parameter) {
+        var builder = new StringBuilder(parameter.getType().getSimpleName());
+        if(parameter.isVarArgs()) builder.append("...");
+        builder.append(" ").append(parameter.getName());
+        return builder.toString();
+    }
+}
