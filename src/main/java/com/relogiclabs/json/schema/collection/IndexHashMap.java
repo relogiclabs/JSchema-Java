34a703a8a7695e56a36fd779f51e342444625706
Zahid Hossain <60911932+zhossain-info@users.noreply.github.com>
2023-09-23 19:47:25 +0600

    Add object property collection

---
+++ src/main/java/com/relogiclabs/json/schema/collection/IndexHashMap.java
@@ -0,0 +1,137 @@
+package com.relogiclabs.json.schema.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class IndexHashMap<TK, TV extends Keyable<TK>> implements IndexMap<TK, TV> {
+
+    private boolean readOnly;
+    private Map<TK, TV> map;
+    private List<TV> list;
+
+    public IndexHashMap(Collection<? extends TV> c) {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        addAll(c);
+    }
+
+    @Override
+    public TV get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public TV get(TK key) {
+        return map.get(key);
+    }
+
+    @Override
+    public Set<TK> keySet() {
+        return map.keySet();
+    }
+
+    @Override
+    public Collection<TV> values() {
+        return list;
+    }
+
+    @Override
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    @Override
+    public void makeReadOnly() {
+        if(readOnly) return;
+        list = Collections.unmodifiableList(list);
+        map = Collections.unmodifiableMap(map);
+        readOnly = true;
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    @Override
+    public Iterator<TV> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return list.toArray(a);
+    }
+
+    @Override
+    public boolean add(TV value) {
+        if(map.containsKey(value.getKey())) return false;
+        list.add(value);
+        map.put(value.getKey(), value);
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if(o instanceof Keyable<?> value) {
+            boolean result = list.remove(value);
+            if(result) map.remove(value.getKey());
+            return result;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for(Object o : c) if(!contains(o)) return false;
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends TV> c) {
+        boolean result = false;
+        for(TV value : c) result |= add(value);
+        return result;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean result = false;
+        for(Object o : c) result |= remove(o);
+        return result;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean result = false;
+        for(TV value : list) if(!c.contains(value)) result |= remove(value);
+        return result;
+    }
+
+    @Override
+    public void clear() {
+        list.clear();
+        map.clear();
+    }
+}
---
+++ src/main/java/com/relogiclabs/json/schema/collection/Keyable.java
@@ -0,0 +1,5 @@
+package com.relogiclabs.json.schema.collection;
+
+public interface Keyable<TK> {
+    TK getKey();
+}
